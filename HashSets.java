0e0e093b28c24657ab2aa52a81f077bf358d194f
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-13 18:45:47 +0530

    hashsets

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,15 @@
+import java.util.HashSet;
+
 public class HashSets{
+    public static void main(String[] args) {
+        HashSet<Person> h=new HashSet<>();
+        h.add(new Person(1,"pavani",20000));
+        h.add(new Person(2, "ganesh", 30000));
+        h.add(new Person(1,"ramya",34000));
+        for (Person person : h) {
+            System.out.println(person);
+        
+    }
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+}
 }
